6d58f8872ccf7d15232c12596e1e82a634197b69
187J3X1-114514 <smjwoainni@qq.com>
2025-03-23 11:00:58 +0800

    添加加载进度条

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,19 @@ setColorScheme((() => {
 createApp(App).mount('#app')
 import { loadBackground } from './background'
 await loadBackground()
-document.getElementById("app")!.classList.add("done")
+const fontA = new FontFace("HarmonyOS", "url(/HarmonyOS_Sans_SC_Medium.woff2)");
+const fontB = new FontFace("JetBrainsMono", "url(/JetBrainsMono-Medium.ttf)");
+
+document.fonts.add(fontA);
+document.fonts.add(fontB);
+await fontA.load()
+await fontB.load()
+document.fonts.ready.then(() => {
+    document.getElementById("app")!.style.display = "block"
+    document.getElementById("loading")!.classList.add("loadingdone")
+    document.getElementById("app")!.classList.add("done")
+});
+
+
 
 
